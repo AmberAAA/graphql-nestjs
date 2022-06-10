7f0982d2f04f04ff4ec6e556649ad5b08005516a
AmberAAA <anborong@outlook.com>
2022-06-10 18:04:03 +0800

    add puppeteer

---
+++ browser-drive.ts
@@ -0,0 +1,10 @@
+import * as puppeteer from 'puppeteer';
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('https://www.baidu.com');
+  await page.screenshot({ path: 'example.png' });
+
+  await browser.close();
+})();

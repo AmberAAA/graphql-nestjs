3b68e96cc0644586b6654e8a1ce029b3a3599954
AmberAAA <anborong@outlook.com>
2022-07-10 10:35:40 +0800

    env

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,8 +16,8 @@ import { Img } from './imgs/entities/img.entity';
     }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
+      host: process.env.MYSQL_HOST,
+      port: process.env.MYSQL_PORT ? +process.env.MYSQL_PORT : 3306,
       username: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
       database: process.env.MYSQL_DATABASE,
